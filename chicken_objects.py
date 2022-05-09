1c549206a66b6bc05c342de4b1a717f0f373e328
MRCLARK0 <89956420+MRCLARK0@users.noreply.github.com>
2022-05-09 11:11:52 -0400

    Display properties updated
    Change print formatting

--- chicken_objects.py
+++ chicken_objects.py
@@ -13,3 +13,22 @@ strip = Chicken("Strip", 341, 33, 53)
 grilled_filet = Chicken("Grilled Filet", 130, 100, 125)
 grilled_nugget = Chicken("Grilled Nugget", 872, 12, 20)
 
+chicken_options = {
+    1: filet.name,
+    2: spicy.name,
+    3: nugget.name,
+    4: strip.name,
+    5: grilled_filet.name,
+    6: grilled_nugget.name,
+    7: breakfast.name
+}
+
+chicken_objects = {
+    1: filet,
+    2: spicy,
+    3: nugget,
+    4: strip,
+    5: grilled_filet,
+    6: grilled_nugget,
+    7: breakfast
+}
--- credit_main.py
+++ credit_main.py
@@ -1,26 +1,7 @@
 import math
+from os import system, name
 from chicken import *
 from chicken_objects import *
-
-chicken_options = {
-    1: filet.name,
-    2: spicy.name,
-    3: nugget.name,
-    4: strip.name,
-    5: grilled_filet.name,
-    6: grilled_nugget.name,
-    7: breakfast.name
-}
-
-chicken_objects = {
-    1: filet,
-    2: spicy,
-    3: nugget,
-    4: strip,
-    5: grilled_filet,
-    6: grilled_nugget,
-    7: breakfast
-}
 
 menu_options = {
     1: 'Start Program',
@@ -37,8 +18,11 @@ def print_chicken():
     for key in chicken_options.keys():
         print(key, '-', chicken_options[key])
 
-def option1():
+def clear():
+    system('cls' if name == 'nt' else 'clear')
 
+def option1():
+    clear()
     print_chicken()
     submission_amount = 0
     chicken_option = 1
@@ -47,10 +31,10 @@ def option1():
         chicken_option = int(input("Enter chicken type(1-7): "))
     except:
         print("Must be a whole number...")
+        
+    clear()
 
     choice = chicken_objects[chicken_option]
-    
-    print(f"--------{choice.name}--------")
 
     while 1 > submission_amount or 10 < submission_amount:
         try:
@@ -60,6 +44,8 @@ def option1():
 
     weights = []
     pieces = []
+
+    clear()
 
     for x in range(submission_amount):
         weight = 0
@@ -72,6 +58,10 @@ def option1():
 
         piece = int(math.ceil(choice.min_piece_per_case - (weights[x] / choice.average_weight_per_piece)))
         pieces.append(piece)
+
+    clear()
+
+    print(f"--------{choice.name}--------")
 
     for x in range(len(weights)):
         print(f"{x+1}: {weights[x]} grams - {pieces[x]} pieces")
